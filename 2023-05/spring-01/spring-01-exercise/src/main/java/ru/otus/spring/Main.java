0d7ac588b6fd5c95a3b809433ecabbd27c82509f
i.konkin <i.konkin@it-alnc.ru>
2023-07-15 14:36:19 +0300

    simple app using ClassPathXmlApplicationContext and constructor for DI

--- 2023-05/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/Main.java
+++ 2023-05/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/Main.java
@@ -1,17 +1,15 @@
 package ru.otus.spring;
 
-//import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.otus.spring.domain.Person;
+import ru.otus.spring.service.PersonService;
 
 public class Main {
 
     public static void main(String[] args) {
-        // TODO: создайте здесь класс контекста
-
-        // TODO: Получите Person Service
-
-        // Получите Person "Ivan"
-        Person ivan = null;
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        PersonService service = context.getBean(PersonService.class);
+        Person ivan = service.getByName("Ivan");
         System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
     }
 }
