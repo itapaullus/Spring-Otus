08c35d779c1a6812249a4eca6a8e55e96a7ccda6
i.konkin <i.konkin@it-alnc.ru>
2023-07-15 15:04:27 +0300

    simple app using ClassPathXmlApplicationContext and setter for DI

--- 2023-05/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
+++ 2023-05/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
@@ -1,17 +1,22 @@
 package ru.otus.spring.service;
 
 import ru.otus.spring.dao.PersonDao;
+import ru.otus.spring.dao.PersonDaoSimple;
 import ru.otus.spring.domain.Person;
 
 public class PersonServiceImpl implements PersonService {
 
-    private final PersonDao dao;
+    private PersonDao dao;
 
-    public PersonServiceImpl(PersonDao dao) {
-        this.dao = dao;
-    }
+//    public PersonServiceImpl(PersonDao dao) {
+//        this.dao = dao;
+//    }
 
     public Person getByName(String name) {
         return dao.findByName(name);
+    }
+
+    public void setDao(PersonDaoSimple dao) {
+        this.dao = dao;
     }
 }
