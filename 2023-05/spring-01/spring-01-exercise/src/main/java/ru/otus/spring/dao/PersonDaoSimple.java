dc736e8a89cf4df8b6b7cb43de466ae7582375e1
i.konkin <i.konkin@it-alnc.ru>
2023-07-15 15:16:43 +0300

    property into PersonDao

--- 2023-05/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/dao/PersonDaoSimple.java
+++ 2023-05/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/dao/PersonDaoSimple.java
@@ -3,8 +3,12 @@ package ru.otus.spring.dao;
 import ru.otus.spring.domain.Person;
 
 public class PersonDaoSimple implements PersonDao {
-
+    private int age;
     public Person findByName(String name) {
-        return new Person(name, 18);
+        return new Person(name, this.age);
+    }
+
+    public void setDefault_age(String defaultAge) {
+        this.age = Integer.parseInt(defaultAge);
     }
 }
