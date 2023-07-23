0b8b094283a3bf3afd8cd4682e476091d18af11b
i.konkin <i.konkin@it-alnc.ru>
2023-07-23 18:00:47 +0300

    classic main config

--- 2023-05/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/Application.java
+++ 2023-05/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/Application.java
@@ -6,14 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import ru.otus.spring.domain.Person;
 import ru.otus.spring.service.PersonService;
 
-@Configuration
+//@Configuration
 @ComponentScan
-public class Main {
+public class Application {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
-
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         PersonService service = context.getBean(PersonService.class);
-
         Person ivan = service.getByName("Ivan");
         System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
     }
