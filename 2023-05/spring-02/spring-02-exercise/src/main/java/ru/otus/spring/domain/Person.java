c64424bd1a32436e98e81781a5aa287853cd77f7
i.konkin <i.konkin@it-alnc.ru>
2023-07-23 22:30:55 +0300

    annotation + lombok

--- 2023-05/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/domain/Person.java
+++ 2023-05/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/domain/Person.java
@@ -1,14 +1,12 @@
 package ru.otus.spring.domain;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class Person {
 
     private final String name;
     private final int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
 
     public String getName() {
         return name;
--- 2023-05/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
+++ 2023-05/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
@@ -1,19 +1,15 @@
 package ru.otus.spring.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.spring.dao.PersonDao;
 import ru.otus.spring.domain.Person;
 
 @Service("personService")
+@RequiredArgsConstructor
 public class PersonServiceImpl implements PersonService {
 
     private final PersonDao dao;
-
-    @Autowired  // ее тут можно не писать, но если >1 конструктора, то так нужно пометить нужный для спринга
-    public PersonServiceImpl(PersonDao dao) {
-        this.dao = dao;
-    }
 
     public Person getByName(String name) {
         return dao.findByName(name);
